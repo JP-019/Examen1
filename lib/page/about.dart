ffcbef3862ab2784d48eed428c794d30d6919d43
Julio <juliocgp30@gmail.com>
2024-05-17 21:13:03 -0600

    v0.4

--- lib/page/about.dart
+++ lib/page/about.dart
@@ -11,15 +11,43 @@ class AboutState extends State<About> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Bottom Content Example'),
-      ),
-      body: Column(
-        children: [
-          Drawer(
-            backgroundColor: Color.fromARGB(255, 37, 37, 37),
-            width: double.infinity,
-            child: ListView(children: [
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(199, 0, 0, 0),
+          title: const Text(
+            'Libreria',
+            style: TextStyle(
+              fontSize: 24,
+              color: Color.fromARGB(197, 255, 255, 255),
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.end,
+          ),
+          actions: const [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.add,
+                    size: 40,
+                    color: Colors.white,
+                  ), // Icono de suma
+                  SizedBox(width: 8), // Espacio entre los iconos
+                  Icon(
+                    Icons.account_circle,
+                    size: 40,
+                    color: Colors.white,
+                  ), // Icono de perfil
+                ],
+              ),
+            ),
+          ],
+        ),
+        body: Drawer(
+          backgroundColor: Color.fromARGB(255, 37, 37, 37),
+          width: double.infinity,
+          child: ListView(
+            children: [
               ListTile(
                 tileColor: Color.fromARGB(255, 51, 71, 59),
                 title: const Text(
@@ -31,15 +59,13 @@ class AboutState extends State<About> {
               ListTile(
                 tileColor: Color.fromARGB(255, 51, 71, 59),
                 title: const Text(
-                  'Favoritos',
+                  'Faboritos',
                   style: TextStyle(color: Colors.white, fontSize: 20),
                 ),
                 onTap: () {},
               ),
-            ]),
+            ],
           ),
-        ],
-      ),
-    );
+        ));
   }
 }
